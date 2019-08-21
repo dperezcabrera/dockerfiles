544f2361229a7308e834beb2fb9e5b23e0e53794
dperezcabrera <dperezcabrera@gmail.com>
2019-08-21 18:11:56 +0200

    Reducir tamaño de las imagenes generadas para theia-ide

--- ide/theia-ide-java/entrypoint.sh
+++ ide/theia-ide-java/entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-node /home/theia/agent/proxy.js > /tmp/proxy.out &
+nohup node /home/theia/agent/proxy.js > /tmp/proxy.out &
 
-node /home/theia/src-gen/backend/main.js /home/project --hostname=0.0.0.0 --startup-timeout=-1 --inspect=0.0.0.0:9229 > /tmp/theia.out 2>/tmp/theia.err
+nohup node /home/theia/src-gen/backend/main.js /home/project --hostname=0.0.0.0 --startup-timeout=-1 --inspect=0.0.0.0:9229 > /tmp/theia.out
 
